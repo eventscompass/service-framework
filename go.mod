4b587a1ec0afb79e72fe02d8cb2de068343ded59
pi-tau <pi.tau@yahoo.com>
2023-10-26 11:31:16 +0200

    Rest and grpc config is read by the framework
    Rest config and grpc config is read from the environment on starting the
    cloud service.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/eventscompass/service-framework
 go 1.21.2
 
 require (
+	github.com/caarlos0/env/v6 v6.10.1
 	golang.org/x/sync v0.4.0
 	golang.org/x/sys v0.13.0
 	google.golang.org/grpc v1.59.0
--- service/cloudservice.go
+++ service/cloudservice.go
@@ -16,30 +16,26 @@ type CloudService interface {
 	Init(_ context.Context) error
 
 	// REST returns the [http.Handler] that is registered for
-	// this service as well as the config for the http server.
-	// Returns (nil, nil) if the service is not serving http
-	// requests.
-	REST() (http.Handler, *RESTConfig)
+	// this service. Returns nil if the service is not serving
+	// http requests.
+	REST() http.Handler
 
 	// GRPC returns the [grpc.Server] that is registered for
-	// this service as well as the config for that server.
-	// Returns (nil, nil) if the service is not serving gRPC
-	// requests.
-	GRPC() (*grpc.Server, *GRPCConfig)
+	// this service. Returns nil if the service is not serving
+	// grpc requests.
+	GRPC() *grpc.Server
 }
 
 // BaseService is a service implementation, which can be used as a base for
 // other cloud services. The service provides a basic implementation of the
 // [CloudService] interface methods.
 type BaseService struct {
-	// restHandler and restCfg are used to start an http server,
-	// that serves http requests to the rest api of the service.
+	// restHandler is used to start an http server, that serves
+	// http requests to the rest api of the service.
 	restHandler http.Handler
-	restCfg     *RESTConfig
 
 	// grpcServer serves requests to the grpc api of the service.
 	grpcServer *grpc.Server
-	grpcCfg    *GRPCConfig
 }
 
 var _ CloudService = (*BaseService)(nil)
@@ -48,25 +44,17 @@ var _ CloudService = (*BaseService)(nil)
 func (s *BaseService) Init(_ context.Context) error { return nil }
 
 // REST implements the [CloudService] interface.
-func (s *BaseService) REST() (http.Handler, *RESTConfig) { return s.restHandler, s.restCfg }
+func (s *BaseService) REST() http.Handler { return s.restHandler }
 
 // GRPC implements the [CloudService] interface.
-func (s *BaseService) GRPC() (*grpc.Server, *GRPCConfig) { return s.grpcServer, s.grpcCfg }
+func (s *BaseService) GRPC() *grpc.Server { return s.grpcServer }
 
 // RegisterREST takes an [http.Handler] and registers that handler with the
-// service. In addition, the config for the http server must also be provided.
-// This function will simply set the corresponding fields of the [BaseService]
-// so that they are available when calling `service.REST()`.
-func (s *BaseService) RegisterREST(h http.Handler, cfg *RESTConfig) {
-	s.restHandler = h
-	s.restCfg = cfg
-}
+// service. This function will simply set the corresponding field of the
+// [BaseService] so that this field is available when calling `service.REST()`.
+func (s *BaseService) RegisterREST(h http.Handler) { s.restHandler = h }
 
 // RegisterGRPC takes a [grpc.Server] and registers that server with the
-// service. In addition, the config for that server must also be provided.
-// This function will simply set the corresponding fields of the [BaseService]
-// so that they are available when calling `service.GRPC()`.
-func (s *BaseService) RegisterGRPC(srv *grpc.Server, cfg *GRPCConfig) {
-	s.grpcServer = srv
-	s.grpcCfg = cfg
-}
+// service. This function will simply set the corresponding field of the
+// [BaseService] so that this field is available when calling `service.GRPC()`.
+func (s *BaseService) RegisterGRPC(srv *grpc.Server) { s.grpcServer = srv }
